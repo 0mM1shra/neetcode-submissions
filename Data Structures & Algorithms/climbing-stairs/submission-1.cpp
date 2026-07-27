1fb90a30fa8f966a84621d7f93d0397326d032a6
Om Mishra <ommishra07032006@gmail.com>
2026-07-27 07:07:43 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int curr,prev=1,prev2=1;
+        for(int i=2;i<=n;i++){
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
